1415d88d72b47c710fd8654df2c8602477c7036a
maokeyang <maokeyang1031@gmail.com>
2017-04-06 23:52:56 +0800

    laravel extension demo  init

---
+++ src/MD5Hasher.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Laravist\Hasher;
+
+/**
+ * Class MD5Hasher
+ * @package Laravist\Hasher
+ */
+class MD5Hasher
+{
+    /**
+     * @param $value
+     * @param array $option
+     * @return string
+     */
+    public function make($value, array $option = [])
+    {
+        $salt = isset($option['salt']) ? $option['salt'] : '';
+        return hash('md5', $value . $salt);
+    }
+
+    /**
+     * @param $value
+     * @param $hashValue
+     * @param array $option
+     * @return bool
+     */
+    public function check($value, $hashValue, array $option = [])
+    {
+        $salt = isset($option['salt']) ? $option['salt'] : '';
+        return hash('md5', $value . $salt) === $hashValue;
+    }
+}
